b8fc4b8135096fa3412f9c9786fe368afbfcfbb7
Mehakshami <mehakshami11@gmail.com>
2025-04-08 13:43:45 +0500

    templeate task

---
+++ sie dard/index.js
@@ -0,0 +1,40 @@
+
+var a = [5, 4, 3, 2, 1];
+for (var i = 0; i < a.length; i++) {
+    console.log(a[i]);
+}
+var a=[6,7,8,9,10];
+for(var i=0;i<a.length;i++){
+    console.log(a[i])
+}
+
+var a = [11,12,13,14,15];
+for (var i = 0; i < a.length; i++) {
+    console.log(a[i]);
+}
+var a = [16,17,18,19,20];
+for (var i = 0; i < a.length; i++) {
+    console.log(a[i]);
+}
+var a = [21,21,23,24,25];
+    for(var i = 0;i<a.length;i++){
+console.log (a[i])
+    }
+
+var a =[26,27,28,29,30];
+for(var i =0 ;i<a.length;i++){
+    console.log(a[i])
+}
+
+var a = [31,32,33,34,35,36];
+for (var i = 0 ;i<a.lengt ;i++){
+    console.log(a[i])
+}
+var a = [37,38,39,40];
+for(var i = 0 ;i<a.length;i++){
+    console.log(a[i])
+} 
+var a =4[1 ,42,43];
+for(var i=0;i<a.length;i++){
+    console.log(a[i])
+}
